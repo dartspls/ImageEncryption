cbfa41efb36d6af57fe779683ea70a2d6e597767
Daniel Shepherd <darts100@gmail.com>
2022-05-17 18:01:54 +1200

    Added IV generator

--- src/main/java/ImageEncrypt.java
+++ src/main/java/ImageEncrypt.java
@@ -4,4 +4,10 @@ import java.security.SecureRandom;
 
 public class ImageEncrypt {
     private static final String key = "770A8A65DA156D24EE2A093277530142"; // From assignment PDF
+
+    private IvParameterSpec getIV() {
+        byte[] ivBytes = new byte[16]; // 128 bit IV
+        new SecureRandom().nextBytes(ivBytes); // fill byte array with next available random bytes
+        return new IvParameterSpec(ivBytes);
+    }
 }
