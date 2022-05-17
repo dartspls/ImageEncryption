d9d635091c2f2e7ae2264c3af59dc467e76b22d7
Daniel Shepherd <darts100@gmail.com>
2022-05-17 17:55:05 +1200

    Creating java file. Added key.

---
+++ src/main/java/ImageEncrypt.java
@@ -0,0 +1,7 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.SecureRandom;
+
+public class ImageEncrypt {
+    private static final String key = "770A8A65DA156D24EE2A093277530142"; // From assignment PDF
+}
